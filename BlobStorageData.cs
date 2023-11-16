20fbc06b8003102dfcf6309258ee5f5fdc1d2fd6
Mateusz Jachym <mateusz.jachym.44@outlook.com>
2023-11-16 15:58:22 +0100

    Add project files.

---
+++ BlobStorageData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlobStorageOperations
+{
+    static class BlobStorageData
+    {
+        // TODO: Add your connection string here:
+        private static readonly string _connectionString = "";
+        private static readonly string _blobContainerName = "stories";
+        private static readonly string _blobName = "matstory.html";
+
+        public static string ConnectionString
+        {
+            get => _connectionString;
+        }
+
+        public static string BlobContainerName
+        {
+            get => _blobContainerName;
+        }
+
+        public static string BlobName
+        {
+            get => _blobName;
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using Azure;
+
+namespace BlobStorageOperations
+{
+    internal class Program
+    {
+        private async static Task Main(string[] args)
+        {
+            try
+            {
+                await CreateCOntainerAndUploadBlobAsync();
+
+            }
+            catch (RequestFailedException e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
+
+        }
+
+        private static async Task CreateCOntainerAndUploadBlobAsync()
+        {
+            // Create the Blob Container
+        }
+    }
+}
